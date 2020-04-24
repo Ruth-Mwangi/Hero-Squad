19211d79b91dd49de4a9a6ec99d5043a05514c32
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-24 20:13:08 +0300

    Created spark routers and hbs files

---
+++ src/main/java/App.java
@@ -0,0 +1,33 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("username", request.session().attribute("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/welcome", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            String inputtedUsername = request.queryParams("username");
+            request.session().attribute("username", inputtedUsername);
+            model.put("username", inputtedUsername);
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+
+    }
+}
