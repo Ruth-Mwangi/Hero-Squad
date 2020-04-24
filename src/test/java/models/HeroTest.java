05a3d673148c403f186d4513a4481268539f188f
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-24 14:02:03 +0300

    Added test for creating an instance for class

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,19 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
+
+    @Test
+    public void createInstanceOfHero_true() throws Exception{
+        Hero hero=setUpNewHero();
+        assertEquals(true,hero instanceof Hero);
+    }
+    //helper
+    private Hero setUpNewHero() {
+        return new Hero("Absorbing Man",30,"Absorbing ","Can Absorb evil thought",20,60);
+    }
 
 }
