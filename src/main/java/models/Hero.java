e3464199a0fd50061831aa7b56635375b6b8c778
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-24 13:45:48 +0300

    Created member variables

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
+    private String mName;
+    private int mId;
+    private int mAge;
+    private String mSpecialPower;
+    private  String mWeakness;
+    private int mDefence;
+    private int mAttack;
+    private static ArrayList<Hero> heroes=new ArrayList<Hero>();
+
+
 }
