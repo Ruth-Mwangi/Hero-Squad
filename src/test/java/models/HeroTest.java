f0aff3a0ceac80b37bb0432d35a2cf2f5810a0a5
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-24 14:18:04 +0300

    Added a test to ensure all instaces are contained in the class

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,16 +1,38 @@
 package models;
 
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HeroTest {
 
+    @After
+    public void tearDown() throws Exception {
+        Hero.clearAll();
+    }
+
     @Test
     public void createInstanceOfHero_true() throws Exception{
         Hero hero=setUpNewHero();
         assertEquals(true,hero instanceof Hero);
     }
+
+    @Test
+    public void returnAllInstancesOfHero() throws Exception{
+        Hero hero=setUpNewHero();
+        Hero otherHero=new Hero("Abraxas",60,"Read Minds ","Gets tired fast",20,60);
+        assertEquals(2,Hero.getHeroes().size());
+    }
+
+    @Test
+    public void allInstancesAreContainedInHero() throws Exception{
+        Hero hero=setUpNewHero();
+        Hero otherHero=new Hero("Abraxas",60,"Read Minds ","Gets tired fast",20,60);
+        assertTrue(Hero.getHeroes().contains(hero));
+        assertTrue(Hero.getHeroes().contains(otherHero));
+    }
+
     //helper
     private Hero setUpNewHero() {
         return new Hero("Absorbing Man",30,"Absorbing ","Can Absorb evil thought",20,60);
