2874bbcd5f9814b3edc066b2ba0c5bf2e1aaff29
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-24 14:56:51 +0300

    Created member variables for Squad class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,13 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Squad {
+
+    private int mId;
+    private int mMaxSize;
+    private String mName;
+    private String mCause;
+    private ArrayList<String> heroes;
+
 }
